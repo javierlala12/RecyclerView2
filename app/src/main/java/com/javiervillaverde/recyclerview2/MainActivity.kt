4c8064075268e2bfdf66c6d7538f92d19a4ae789
javierlala12 <javiervillaverde71@gmail.com>
2023-10-09 09:27:45 +0200

    update

--- app/src/main/java/com/javiervillaverde/recyclerview2/MainActivity.kt
+++ app/src/main/java/com/javiervillaverde/recyclerview2/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         // Agrega ciudades a la lista
-        ciudades.add(Ciudad("Madrid", "https://www.spain.info/.content/imagenes/cabeceras-grandes/madrid/calle-gran-via-madrid-s333961043.jpg"))
+        ciudades.add(Ciudad("Madrid", "https://media.gettyimages.com/id/170131240/es/foto/skyline-of-madrid-with-metropolis-building-and-gra.jpg?s=612x612&w=gi&k=20&c=frPS4I4fdMJIPpb8XAOZPslXfDYsUXitdTPKI3WuRy8="))
         ciudades.add(Ciudad("Barcelona", "https://media.traveler.es/photos/63838947050e0f92cd80c982/16:9/w_2560%2Cc_limit/GettyImages-1392907424.jpg"))
         ciudades.add(Ciudad("Valencia", "https://www.floresyplantas.net/wp-content/uploads/ciudad-de-valencia-y-su-rio-turia.jpg"))
 
